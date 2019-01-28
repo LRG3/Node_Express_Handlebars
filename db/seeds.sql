b49f492c2d4e09c95535aea8ca279120c4800b75
LRG3 <lrg2013@yahoo.com>
2019-01-27 23:39:25 -0700

    update 5

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Select burgers
+USE burgers_db;
+
+-- insert 3 burgers
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deer', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom', false);
